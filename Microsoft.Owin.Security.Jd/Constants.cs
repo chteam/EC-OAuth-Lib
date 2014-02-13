532a08473841da1bf91b1bcf0bbaa97f0a9a7485
chsword <chsword@126.com>
2014-02-13 11:02:11 +0800

    提交

--- Microsoft.Owin.Security.Jd/Constants.cs
+++ Microsoft.Owin.Security.Jd/Constants.cs
@@ -2,8 +2,8 @@
 {
     internal static class Constants
     {
-        internal const string AuthizationEndpoint = "https://auth.360buy.com/oauth/authorize";
-        internal const string TokenEndpoint = "https://auth.360buy.com/oauth/token";
+        internal const string AuthizationEndpoint = "http://auth.360buy.com/oauth/authorize";
+        internal const string TokenEndpoint = "http://auth.360buy.com/oauth/token";
         internal const string DefaultAuthenticationType = "JingDongAccount";
     }
 }
